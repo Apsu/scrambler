63d9bffe45d4b9bc50496e0982f206f1f4df457a
Evan Callicoat <apsu@propter.net>
2014-02-20 00:35:13 -0600

    Add entry point wrapper

---
+++ scram.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import scram
+
+
+# Entry point
+if __name__ == "__main__":
+    scram.main("eth2")
--- scram/__init__.py
+++ scram/__init__.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python
-
 from scram.cluster import Cluster
 
+
 # Entry point
-if __name__ == "__main__":
-    # Join and track cluster
-    Cluster(interface="eth2")
+def main(interface):
+    # Start cluster agent
+    Cluster(interface=interface)
