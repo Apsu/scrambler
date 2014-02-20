12f4b2b9f5eea5dc3118d498cce11709a69713af
Evan Callicoat <apsu@propter.net>
2014-02-20 01:05:50 -0600

    Fixup main and __main__ for argv

--- scram/__init__.py
+++ scram/__init__.py
@@ -1,7 +1,13 @@
+import sys
+
 from scram.cluster import Cluster
 
 
 # Entry point
-def main(interface):
+def main(argv=sys.argv[1:]):
     # Start cluster agent
-    Cluster(interface=interface)
+    Cluster(interface=argv[0])
+
+# If called directly
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))  # Hope for the best
