c1948b1fdd80b7fa7b60b83b17a19222baa7a6c5
Evan Callicoat <apsu@propter.net>
2014-02-24 15:08:51 -0600

    Fixes #5

--- scrambler/config.py
+++ scrambler/config.py
@@ -1,5 +1,7 @@
 import json
 
+from scrambler.synchronized import synchronized
+
 
 class Config():
     "JSON config file object"
@@ -9,23 +11,27 @@ class Config():
         self.path = path
         self.read()
 
+    @synchronized("write")
     def read(self):
         "Parse that sucker"
 
         with open(self.path, "r") as fd:
             self.config = json.load(fd)
 
+    @synchronized("read")
     def write(self):
         "Write out our object"
 
         with open(self.path, "w") as fd:
             json.dump(self.config, fd)
 
+    @synchronized("read")
     def __getitem__(self, key):
         "Get a copy of the requested key's value or None if not found"
 
         return self.config[key].copy() if key in self.config else None
 
+    @synchronized("write")
     def __setitem__(self, key, value):
         "Push the new value in"
 
