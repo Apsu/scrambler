4cfbf6bf8a89d19d3f4f5c0a8a1a148d125a9eac
Evan Callicoat <apsu@propter.net>
2014-02-24 13:33:46 -0600

    Add comments

--- scrambler/docker.py
+++ scrambler/docker.py
@@ -2,22 +2,38 @@ import docker
 
 
 class Docker():
+    "Docker binding interface"
+
     def __init__(self):
+        # Create client
         self.client = docker.Client()
 
     def containers_by_image(self):
+        "Return containers indexed by image name, with count"
+
+        # Dat declaration
         results = {}
+
+        # Flat list of running container dicts
         containers = self.client.containers()
+
+        # Walk the list
         for container in containers:
+            # Snag container image
             image = container["Image"]
+
+            # If we've seen this one already, +1 the count
             if image in results:
                 results[image]["Count"] += 1
+            # Otherwise add the first one
             else:
-                results[image] = {"Count": 1}
+                results[image] = {
+                    "Count": 1,
+                    "Containers": []
+                }
 
-            if "Containers" in results[image]:
-                results[image]["Containers"].append(container)
-            else:
-                results[image]["Containers"] = [container]
+            # Add container to list
+            results[image]["Containers"].append(container)
 
+        # Pop 'em out
         return results
