7c088a20237efc6d5c60cd4c2cb1c0bacb5f133e
Evan Callicoat <apsu@propter.net>
2014-02-28 11:28:11 -0600

    Prefix variables to fix digest() name overlap

--- scrambler/auth.py
+++ scrambler/auth.py
@@ -6,18 +6,18 @@ class Auth():
 
     def __init__(self, key, data):
         # Store parameters
-        self.key = key
-        self.data = data
+        self._key = key
+        self._data = data
 
         # Compute this node's digest
-        self.digest = hmac.new(str(self.key), str(self.data)).hexdigest()
+        self._digest = hmac.new(str(self._key), str(self._data)).hexdigest()
 
     def digest(self):
         "Get our digest"
 
-        return self.digest
+        return self._digest
 
     def verify(self, digest, data):
         "Verify specified digest matches data with our key"
 
-        return hmac.new(str(self.key), str(data)).hexdigest() == digest
+        return hmac.new(str(self._key), str(data)).hexdigest() == digest
